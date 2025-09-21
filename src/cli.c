6d1bc5766d716d0786461118592d2eb02a1b24ed
Awe Morris <awe@noctvm.io>
2025-09-21 17:48:54 +0900

    Improve print()

--- src/cli.c
+++ src/cli.c
@@ -54,6 +54,7 @@ static bool accept_multiline(const char *text);
 static bool load_file_content(const char *fname, char **data, size_t *size);
 static int wide_printf(const char *format, ...);
 static bool register_ffi(NoctEnv *env);
+static bool serialize_printer(NoctEnv *env, char *buf, size_t size, NoctValue *value, bool is_inside_obj);
 
 /*
  * Main
@@ -1037,36 +1038,102 @@ static bool
 cfunc_print(
 	struct rt_env *env)
 {
-	NoctValue msg;
-	const char *s;
-	float f;
-	int i;
-	int type;
+	char buf[8192];
+	NoctValue value;
 
-	if (!noct_get_arg(env, 0, &msg))
+	if (!noct_pin_local(env, 1, &value))
 		return false;
 
-	if (!noct_get_value_type(env, &msg, &type))
+	if (!noct_get_arg(env, 0, &value)) {
+		noct_unpin_local(env, 1, &value);
+		return false;
+	}
+
+	memset(buf, 0, sizeof(buf));
+	serialize_printer(env, buf, sizeof(buf), &value, false);
+
+	wide_printf("%s\n", buf);
+
+	noct_unpin_local(env, 1, &value);
+
+	return true;
+}
+	
+static bool serialize_printer(NoctEnv *env, char *buf, size_t size, NoctValue *value, bool is_inside_obj)
+{
+	int type;
+	int ival;
+	float fval;
+	const char *sval;
+	int items;
+	int i;
+	char digits[1024];
+
+	if (!noct_get_value_type(env, value, &type))
 		return false;
 
 	switch (type) {
 	case NOCT_VALUE_INT:
-		if (!noct_get_int(env, &msg, &i))
+		if (!noct_get_int(env, value, &ival))
 			return false;
-		wide_printf("%i\n", i);
+		snprintf(digits, sizeof(digits), "%d", ival);
+		strncat(buf, digits, size);
 		break;
 	case NOCT_VALUE_FLOAT:
-		if (!noct_get_float(env, &msg, &f))
+		if (!noct_get_float(env, value, &fval))
 			return false;
-		wide_printf("%f\n", f);
+		snprintf(digits, sizeof(digits), "%f", fval);
+		strncat(buf, digits, size);
 		break;
 	case NOCT_VALUE_STRING:
-		if (!noct_get_string(env, &msg, &s))
+		if (!noct_get_string(env, value, &sval))
 			return false;
-		wide_printf("%s\n", s);
+		if (is_inside_obj)
+			strncat(buf, "\"", size);
+		strncat(buf, sval, size);
+		if (is_inside_obj)
+			strncat(buf, "\"", size);
+		break;
+	case NOCT_VALUE_ARRAY:
+		if (!noct_get_array_size(env, value, &items))
+			return false;
+		strncat(buf, "[", size);
+		for (i = 0; i < items; i++) {
+			NoctValue elem;
+			if (!noct_get_array_elem(env, value, i, &elem))
+				return false;
+			if (!serialize_printer(env, buf, size, &elem, true))
+				return false;
+			if (i != items - 1)
+				strncat(buf, ", ", size);
+		}
+		strncat(buf, "]", size);
+		break;
+	case NOCT_VALUE_DICT:
+		if (!noct_get_dict_size(env, value, &items))
+			return false;
+		strncat(buf, "{", size);
+		for (i = 0; i < items; i++) {
+			NoctValue k, v;
+			if (!noct_get_dict_key_by_index(env, value, i, &k))
+				return false;
+			if (!noct_get_string(env, &k, &sval))
+				return false;
+			strncat(buf, sval, size);
+			strncat(buf, ": ", size);
+			if (!noct_get_dict_value_by_index(env, value, i, &v))
+				return false;
+			serialize_printer(env, buf, size, &v, true);
+			if (i != items - 1)
+				strncat(buf, ", ", size);
+		}
+		strncat(buf, "}", size);
+		break;
+	case NOCT_VALUE_FUNC:
+		strncat(buf, "<func>", size);
 		break;
 	default:
-		wide_printf("[object]\n");
+		assert(0);
 		break;
 	}
 
