7d43418257cba07081e7b5a8e8dc97be3e9edb1d
Awe Morris <awe@noctvm.io>
2025-10-09 19:12:07 +0900

    Rename the noct command to noctcli

--- tests/run-syntax.sh
+++ tests/run-syntax.sh
@@ -9,13 +9,13 @@ echo 'Running bootstrap tests...'
 echo "(Interpreter)";
 for tc in syntax/*.noct; do
     echo "$tc";
-    ../build/noct --disable-jit $tc > out || true;
+    ../build/noctcli --disable-jit $tc > out || true;
     diff $tc.out out;
 done
 echo "(JIT)";
 for tc in syntax/*.noct; do
     echo "$tc";
-    ../build/noct --force-jit $tc > out || true;
+    ../build/noctcli --force-jit $tc > out || true;
     diff $tc.out out;
 done
 echo 'All tests passed.'
